146c630e49a10063fc3d00890b097f1fe6962698
Murat Kara <muratkarazade@gmail.com>
2023-03-11 10:26:46 +0300

    cleared

--- Enoca.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Enoca.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
